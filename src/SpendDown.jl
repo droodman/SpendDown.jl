714fba27a6018dd2eae6b47124ac1d577253967e
droodman <droodman@gmail.com>
2026-09-16 16:56:28 -0400

    debug

--- src/SpendDown.jl
+++ src/SpendDown.jl
@@ -2,17 +2,17 @@ module SpendDown
 
 export SimModel, sim, SimModelOld, simOld, TwoPieceUniform, PiecewiseUniform, Bitriangular, rhist
 
-using Random, Distributions, Statistics, StatsBase, DataFrames, LogExpFunctions, AxisKeys, LinearAlgebra, QuasiMonteCarlo, Pluto
+using Random, Distributions, Statistics, StatsBase, DataFrames, LogExpFunctions, AxisKeys, LinearAlgebra, QuasiMonteCarlo #, Pluto
 
 include("PriorDistributions.jl")
 include("SpendDownv1.jl")
 
-public notebook
-function notebook()
-  target = joinpath(mktempdir(), "frontend.jl")
-  cp(joinpath(dirname(@__FILE__), "frontend.jl"), target; force=true)
-  Pluto.run(notebook=target)
-end
+# public notebook
+# function notebook()
+#   target = joinpath(mktempdir(), "frontend.jl")
+#   cp(joinpath(dirname(@__FILE__), "frontend.jl"), target; force=true)
+#   Pluto.run(notebook=target)
+# end
 
 const rhist = [  # real January-January S&P returns, 1871-2024, computed from Shiller's ie_data.xls, https://shillerdata.com, Data tab, col. J
                 1.139054
